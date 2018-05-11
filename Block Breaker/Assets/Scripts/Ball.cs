16152c8f64c7dd9a68780c58b28606b2d58bb961
Nubiee <521310kevin@gmail.com>
2018-05-10 23:13:48 -0500

    First try

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle paddle;
+    private Vector3 paddleToBallVector;
+    private bool hasStarted = false;
+	// Use this for initialization
+	void Start ()
+    {
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+        print(paddleToBallVector);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasStarted)
+        {   
+            //lock paddle and ball
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+            //wait for mouse to be clicked
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("Mouse clicked, launch ball");
+                hasStarted = true;
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+            }
+        }
+	}
+}
---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager levelmanager;
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        print("Trigger");
+        levelmanager.LoadLevel("Win");
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+        levelmanager.LoadLevel("Win");
+    }
+
+
+}
